8f5c67bb0c1b20dbc0d6eb7f3a5c96c339787533
JAG24_Exo <joseguillen587@yahoo.com>
2024-07-11 13:12:26 -0700

    test js for photo viewer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// Blank JS template for now
+// Photo viewer
+
+function openPhotoViewer(imgElement) {
+    const viewer = document.getElementById("photo-viewer");
+    const viewerImg = document.getElementById("viewer-img");
+    const viewerCaption = document.getElementById("viewer-caption");
+
+    viewer.style.display = "block";
+    viewerImg.src = imgElement.src;
+    viewerCaption.innerHTML = imgElement.alt;
+}
+
+function closePhotoViewer() {
+    const viewer = document.getElementById("photo-viewer");
+    viewer.style.display = "none";
+}
+
+
+
+// contact form WIP
+
