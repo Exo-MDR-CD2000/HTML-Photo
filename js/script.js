af85a7e45f36857141ec4229cade1d87340e2615
JAG24_Exo <joseguillen587@yahoo.com>
2024-07-11 13:52:35 -0700

    photo viewer wip

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-// Photo viewer
+document.querySelectorAll('.photo-item').forEach(item => {
+    item.addEventListener('click', function() {
+        var modal = document.getElementById('photoViewerModal');
+        var modalImg = document.getElementById('modalImg');
+        var captionText = document.getElementById('caption');
+        var img = this.querySelector('img'); // Find the img element within the clicked .photo-item
+        modal.style.display = "block";
+        modalImg.src = img.src; // Use the src of the img found
+        captionText.innerHTML = img.alt; // Use the alt text of the img found as caption
+    });
+});
 
-function openPhotoViewer(imgElement) {
-    const viewer = document.getElementById("photo-viewer");
-    const viewerImg = document.getElementById("viewer-img");
-    const viewerCaption = document.getElementById("viewer-caption");
+// Get the <span> element that closes the modal
+var close = document.getElementsByClassName("close")[0];
 
-    viewer.style.display = "block";
-    viewerImg.src = imgElement.src;
-    viewerCaption.innerHTML = imgElement.alt;
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+    var modal = document.getElementById('photoViewerModal');
+    modal.style.display = "none";
 }
-
-function closePhotoViewer() {
-    const viewer = document.getElementById("photo-viewer");
-    viewer.style.display = "none";
-}
-
 
 
 // contact form WIP
